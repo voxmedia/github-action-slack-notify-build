22edba3d0ba203d50a2e11aafe8ce704df6f2614
Josh Larson <josh.larson@voxmedia.com>
2019-10-24 15:49:43 -0500

    Give this a go

--- scripts/report-ci-failure.js
+++ scripts/report-ci-failure.js
@@ -1,3 +1,3 @@
 const results = require('../results.json');
 
-console.log(`::set-output name=results,::${results.testResults.message}`);
+console.log(`::set-output name=results,::${results.testResults[0].message}`);
