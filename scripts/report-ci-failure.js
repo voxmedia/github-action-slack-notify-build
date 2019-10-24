8678ff4595b6b34f78caccb44f50ac4e72e922aa
Josh Larson <josh.larson@voxmedia.com>
2019-10-24 16:07:33 -0500

    Fix this issue

--- scripts/report-ci-failure.js
+++ scripts/report-ci-failure.js
@@ -1,4 +1,4 @@
 const results = require('../results.json');
-const text = `::set-output name=results,::${numFailedTestSuites} test failure(s):\n\n\`\`\`${results.testResults[0].message}\`\`\``;
+const text = `::set-output name=results,::${results.numFailedTestSuites} test failure(s):\n\n\`\`\`${results.testResults[0].message}\`\`\``;
 
 process.stdout.write(text.replace(/\r/g, '%0D').replace(/\n/g, '%0A'));
