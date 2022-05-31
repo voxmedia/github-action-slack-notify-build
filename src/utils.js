e0621d6fc7d082aab475c0b8ea8508264b554746
RyanLaube <ryan@prairierobotics.com>
2022-05-31 10:32:30 -0600

    change logs

--- src/utils.js
+++ src/utils.js
@@ -6,7 +6,7 @@ function buildSlackAttachments({ status, color, github }) {
   const event = eventName;
   const branch = event === 'pull_request' ? payload.pull_request.head.ref : ref.replace('refs/heads/', '');
 
-  console.log(event.context.payload);
+  console.log(github.context);
 
   const sha = event === 'pull_request' ? payload.pull_request.head.sha : github.context.sha;
   const runId = parseInt(process.env.GITHUB_RUN_ID, 10);
