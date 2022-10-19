8734800ec6ce5119b45ea37d518ab2810a324c0d
Eric Musliner <eric.musliner@core4ce.com>
2022-10-19 15:32:29 -0400

    Adds Job field

--- src/utils.js
+++ src/utils.js
@@ -8,6 +8,7 @@ function buildSlackAttachments({ status, color, github }) {
 
   const sha = event === 'pull_request' ? payload.pull_request.head.sha : github.context.sha;
   const runId = parseInt(process.env.GITHUB_RUN_ID, 10);
+  const job = process.env.GITHUB_JOB;
 
   const referenceLink =
     event === 'pull_request'
@@ -35,6 +36,11 @@ function buildSlackAttachments({ status, color, github }) {
           title: 'Workflow',
           value: `<https://github.com/${owner}/${repo}/actions/runs/${runId} | ${workflow}>`,
           short: true,
+        },
+        {
+          title: 'Job',
+          value: `${job}`,
+          short: true
         },
         {
           title: 'Status',
